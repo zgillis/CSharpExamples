13893b5c1aab12b81f804af6e6af502be3049da9
root <root@vps196815.vps.ovh.ca>
2018-10-05 15:01:46 -0400

    First Demo

---
+++ CSLearn1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CSLearn1
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("---PROGRAM STARTED---");
+			DoAsyncStuff ();
+			for(int i = 0; i < 10; i++) {
+				Console.WriteLine("[MAIN]: " + i);
+				System.Threading.Thread.Sleep(500);
+			}
+		}
+
+		public static async Task DoAsyncStuff()
+		{
+			await Task.Run (() => {
+				for(int i = 0; i < 10; i++) {
+					Console.WriteLine("[DoAsyncStuff]: " + i);
+					System.Threading.Thread.Sleep(500);
+				}
+			});
+		}
+	}
+}
